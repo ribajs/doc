b422b8447bc013eab56ff486afa23ab0ed6921bc
JumpLink <pascal@jumplink.eu>
2019-10-04 21:20:19 +0200

    Black example tabs

--- src/ts/components/example-tabs/example-tabs.component.ts
+++ src/ts/components/example-tabs/example-tabs.component.ts
@@ -1,5 +1,6 @@
 import {
   Debug,
+  Utils,
 } from '@ribajs/core';
 
 import {
@@ -20,6 +21,7 @@ export class ExampleTabsComponent extends TabsComponent {
 
   constructor(element?: HTMLElement) {
     super(element);
+    this.setCodeContentOfTabs();
     this.init(ExampleTabsComponent.observedAttributes);
   }
 
@@ -38,14 +40,34 @@ export class ExampleTabsComponent extends TabsComponent {
     Prism.highlightAll();
   }
 
+  protected setCodeContentOfTabs() {
+    for (const tab of this.scope.tabs) {
+      if (tab.title.endsWith('.ts')) {
+        tab.content = `<pre class="language-typescript"><code class="language-typescript">${tab.content}</code></pre>`;
+      }
+      if (tab.title.endsWith('.html')) {
+        tab.content = `<pre class="language-html"><code class="language-html">${Utils.escapeHtml(tab.content)}</code></pre>`;
+      }
+      if (tab.title.endsWith('.scss')) {
+        tab.content = `<pre class="language-scss"><code class="language-scss">${tab.content}</code></pre>`;
+      }
+      if (tab.title.endsWith('.js')) {
+        tab.content = `<pre class="language-javascript"><code class="language-javascript">${tab.content}</code></pre>`;
+      }
+      if (tab.title.endsWith('.pug')) {
+        tab.content = `<pre class="language-pug"><code class="language-pug">${tab.content}</code></pre>`;
+      }
+    }
+  }
+
   protected template() {
-    // Only set the component template if there no childs already
-    if (this.el.hasChildNodes()) {
-      this.debug('Do not use template, because element has child nodes');
-      return null;
-    } else {
+    // Only set the component template if there no childs or the childs are templates
+    if (!this.el.hasChildNodes() || this.onlyTemplateChilds()) {
       this.debug('Use template', template);
       return template;
+    } else {
+      this.debug('Do not use template, because element has child nodes');
+      return null;
     }
   }
 }
